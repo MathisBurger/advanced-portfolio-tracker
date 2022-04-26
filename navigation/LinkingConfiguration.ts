7494a566a3b8d74fe367499f59de451e4a131849
Mathis Burger <mburger@die-netzwerkstatt.de>
2022-04-26 17:30:48 +0200

    [TASK] clened app

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -15,19 +15,13 @@ const linking: LinkingOptions<RootStackParamList> = {
     screens: {
       Root: {
         screens: {
-          TabOne: {
+          Home: {
             screens: {
-              TabOneScreen: 'one',
-            },
-          },
-          TabTwo: {
-            screens: {
-              TabTwoScreen: 'two',
+              HomeScreen: 'home',
             },
           },
         },
       },
-      Modal: 'modal',
       NotFound: '*',
     },
   },
